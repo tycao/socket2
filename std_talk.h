a97d0e38844b8ca8e4d8ba29a6048177d39d5ff9
tycao <616881845@qq.com>
2017-07-06 23:07:51 +0800

    把需要用到的C++库（STL库和boost库）放在此.h头文件里

---
+++ std_talk.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+using namespace std;
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+using namespace boost;
+using namespace boost::asio;
